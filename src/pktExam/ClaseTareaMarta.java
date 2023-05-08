db48dad5b74631ac9eb28858210094b5e33b9f93
mtirador <125604629+mtirador@users.noreply.github.com>
2023-05-08 09:43:03 +0200

    modificado marta clase nueva

---
+++ src/pktExam/ClaseTareaMarta.java
@@ -0,0 +1,11 @@
+package pktExam;
+
+public class ClaseTareaMarta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola Irene, estoy haciendo la práctica");
+	}
+
+}
